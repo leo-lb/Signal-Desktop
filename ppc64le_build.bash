2e50516ca315e17662f719796c48f2b2b941a70b
Leo Le Bouter <lle-bout@zaclys.net>
2019-06-20 14:37:30 +0200

    Compile phantomjs

--- ppc64le_build.bash
+++ ppc64le_build.bash
@@ -9,7 +9,31 @@ wget "https://nodejs.org/dist/$VERSION/node-$VERSION-$DISTRO.tar.xz"
 tar -xJvf node-$VERSION-$DISTRO.tar.xz
 PATH=$(pwd)/node-$VERSION-$DISTRO/bin:$PATH
 
-sudo npm install -g yarn gulp electron-rebuild
+which yarn || sudo npm install -g yarn
+which gulp || sudo npm install -g gulp
+which electron-rebuild || sudo npm install -g electron-rebuild
+
+OLDDIR="$(pwd)"
+
+echo "#######################"
+echo "Signal needs svg2png which requires phantomjs, we're going to compile it from source."
+echo "It needs several build dependencies, check for them here: http://phantomjs.org/build.html"
+echo "Please note that it requires OpenSSL 1.0"
+echo "For a recent Fedora system, run: sudo yum dnf gcc gcc-c++ make flex bison gperf ruby compat-openssl10-devel.ppc64le freetype-devel fontconfig-devel libicu-devel sqlite-devel libpng-devel libjpeg-devel"
+echo "#######################"
+
+cd /tmp
+git clone git://github.com/ariya/phantomjs.git
+cd phantomjs
+git checkout 2.1.1
+git submodule init
+git submodule update
+
+CXXFLAGS="-fpermissive" python build.py
+
+PATH="$(pwd)/bin:$PATH"
+
+cd "$OLDDIR"
 
 yarn install
 yarn grunt
